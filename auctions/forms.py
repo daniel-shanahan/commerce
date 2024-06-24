69f235d00a92f980ffb7aeaaf34fd6ac07a3d8f5
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2024-06-23 20:55:50 -0400

    Added categories to Listing model

--- auctions/forms.py
+++ auctions/forms.py
@@ -6,7 +6,7 @@ from .models import Listing
 class ListingForm(ModelForm):
     class Meta:
         model = Listing
-        fields = ["title", "description", "starting_bid", "image_url"]
+        fields = ["title", "description", "starting_bid", "image_url", "category"]
         widgets = {
             "description": Textarea(
                 attrs={"cols": 50, "rows": 2, "class": "form-control mb-3"}
@@ -17,4 +17,5 @@ class ListingForm(ModelForm):
         super().__init__(*args, **kwargs)
         self.fields["title"].widget.attrs.update({"class": "form-control mb-3"})
         self.fields["starting_bid"].widget.attrs.update({"class": "form-control mb-3"})
-        self.fields["image_url"].widget.attrs.update({"class": "form-control mb-4"})
+        self.fields["image_url"].widget.attrs.update({"class": "form-control mb-3"})
+        self.fields["category"].widget.attrs.update({"class": "form-control mb-3"})
--- auctions/models.py
+++ auctions/models.py
@@ -7,10 +7,26 @@ class User(AbstractUser):
 
 
 class Listing(models.Model):
+    ELECTRONICS = "E"
+    FASHION = "F"
+    HOME = "H"
+    TOYS = "T"
+    CATEGORY_CHOICES = {
+        ELECTRONICS: "Electronics",
+        FASHION: "Fashion",
+        HOME: "Home",
+        TOYS: "Toys",
+    }
+
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
     starting_bid = models.DecimalField(max_digits=9, decimal_places=2)
     image_url = models.URLField(blank=True, default="", verbose_name="Image URL")
+    category = models.CharField(
+        max_length=1,
+        choices=CATEGORY_CHOICES,
+        default=ELECTRONICS,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
