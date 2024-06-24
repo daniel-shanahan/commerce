585ed5ae8bc5e07544f28529adc221ec89fc9606
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2024-06-24 17:26:20 -0400

    Added Bid model

--- auctions/models.py
+++ auctions/models.py
@@ -35,3 +35,14 @@ class Listing(models.Model):
 
     def __str__(self):
         return f"{self.title} (${self.starting_bid})"
+
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    listing = models.ForeignKey(
+        Listing, on_delete=models.CASCADE, null=False, related_name="bids"
+    )
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="bids", null=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
